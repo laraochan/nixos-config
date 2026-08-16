9acf57b4e4133560592c9af712eb97f11f819c7c
larao <me@larao.dev>
2026-08-17 04:51:47 +0900

    feat: manage VS Code declaratively

--- home/vscode.nix
+++ home/vscode.nix
@@ -1,5 +1,26 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  programs.vscode.enable = true;
+  programs.vscode = {
+    enable = true;
+
+    # Keep extensions reproducible. Add or remove extensions here instead of
+    # installing them from VS Code's Extensions view.
+    mutableExtensionsDir = false;
+
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        astro-build.astro-vscode
+        jnoortheen.nix-ide
+        mvllow.rose-pine
+        rust-lang.rust-analyzer
+      ];
+
+      userSettings = {
+        "git.autofetch" = true;
+        "workbench.startupEditor" = "none";
+        "workbench.colorTheme" = "Rosé Pine";
+      };
+    };
+  };
 }
